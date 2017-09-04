2d7390f01edf17b9eb24f450c7dfba3441c85751
Quincy <hu.quincy@outlook.com>
2017-09-04 17:42:19 +0800

    New Project

---
+++ CalibreTools/CalibreTools.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace CalibreTools
+{
+    partial class CalibreTools
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonMove = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.listViewBooks = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.openFileDialogLib = new System.Windows.Forms.OpenFileDialog();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialogLib = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(646, 382);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonMove);
+            this.tabPage1.Controls.Add(this.buttonRefresh);
+            this.tabPage1.Controls.Add(this.listViewBooks);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(638, 356);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "库文件";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonMove
+            // 
+            this.buttonMove.Location = new System.Drawing.Point(555, 47);
+            this.buttonMove.Name = "buttonMove";
+            this.buttonMove.Size = new System.Drawing.Size(75, 23);
+            this.buttonMove.TabIndex = 1;
+            this.buttonMove.Text = "移动";
+            this.buttonMove.UseVisualStyleBackColor = true;
+            this.buttonMove.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(555, 18);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 1;
+            this.buttonRefresh.Text = "刷新";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // listViewBooks
+            // 
+            this.listViewBooks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewBooks.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listViewBooks.FullRowSelect = true;
+            this.listViewBooks.Location = new System.Drawing.Point(3, 3);
+            this.listViewBooks.Name = "listViewBooks";
+            this.listViewBooks.Size = new System.Drawing.Size(472, 350);
+            this.listViewBooks.TabIndex = 0;
+            this.listViewBooks.UseCompatibleStateImageBehavior = false;
+            this.listViewBooks.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "标题";
+            this.columnHeader1.Width = 167;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "路径";
+            this.columnHeader2.Width = 297;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(638, 356);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "文档目录";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "库路径:";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(61, 386);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(388, 21);
+            this.textBoxPath.TabIndex = 2;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(559, 386);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "打开";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(559, 412);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "关闭";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // openFileDialogLib
+            // 
+            this.openFileDialogLib.FileName = "*.db";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(449, 385);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(30, 23);
+            this.buttonBrowse.TabIndex = 4;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(638, 356);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "下载";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // CalibreTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 438);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "CalibreTools";
+            this.Text = "Calibre工具";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CalibreTools_FormClosing);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.ListView listViewBooks;
+        private System.Windows.Forms.Button buttonMove;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.OpenFileDialog openFileDialogLib;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogLib;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ CalibreTools/CalibreTools.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace CalibreTools
+{
+    public partial class CalibreTools : Form
+    {
+        private SQLiteConnection m_dbConnection = null;
+        private String m_strCalibreRoot = "";
+
+        public CalibreTools()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialogLib.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBoxPath.Text = openFileDialogLib.FileName;
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (m_dbConnection != null)
+            {
+                m_dbConnection.Close();
+                m_dbConnection = null;
+            }
+
+            m_dbConnection = new SQLiteConnection(String.Format("Data Source={0}",textBoxPath.Text));
+            m_dbConnection.Open();
+
+            try
+            {
+                string sql = "DROP TRIGGER books_update_trg;";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+            }
+
+            m_strCalibreRoot = Path.GetDirectoryName(textBoxPath.Text);
+
+            MessageBox.Show("打开操作成功，操作前请先关闭Calibre应用");
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            if (m_dbConnection != null)
+            {
+                try
+                {
+                    string sql =
+                    "CREATE TRIGGER books_update_trg AFTER UPDATE ON books " +
+                    "BEGIN " +
+                        "UPDATE books SET sort=title_sort(NEW.title) WHERE id=NEW.id AND OLD.title <> NEW.title;" +
+                    "END";
+                    SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                }
+
+                m_dbConnection.Close();
+                m_dbConnection = null;
+
+                MessageBox.Show("关闭操作成功");
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            listViewBooks.Items.Clear();
+
+            string sql = "select id,title,path from books";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ListViewItem item = new ListViewItem(
+                    new string[]
+                    {
+                        reader["title"] as string,
+                        reader["path"] as string
+                    });
+                item.Tag = reader["id"];
+                listViewBooks.Items.Add(item);
+            }
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string strMoveFolder = "";
+            if (listViewBooks.SelectedItems.Count > 0)
+            {
+                folderBrowserDialogLib.SelectedPath = m_strCalibreRoot;
+                DialogResult result = folderBrowserDialogLib.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    strMoveFolder = folderBrowserDialogLib.SelectedPath;
+                }
+            }
+
+            if (string.IsNullOrEmpty(strMoveFolder))
+            {
+                return;
+            }
+
+            if (!strMoveFolder.StartsWith(m_strCalibreRoot))
+            {
+                return;
+            }
+
+            string newPathValue = strMoveFolder.Remove(0, m_strCalibreRoot.Length+1);
+            newPathValue = newPathValue.Replace('\\', '/');
+
+            foreach (ListViewItem item in listViewBooks.SelectedItems)
+            {
+                string sourceFolder = Path.Combine(m_strCalibreRoot,item.SubItems[1].Text);
+                if (Directory.Exists(sourceFolder))
+                {
+                    try
+                    {
+                        string sql = "update books set path='{0}' where id={1}";
+                        // 使用中文名代替拼音
+                        string title = item.SubItems[0].Text;
+                        string parentFolder = "/" + title;
+                        string newPath = newPathValue + parentFolder;
+                        if (Directory.Exists(strMoveFolder + parentFolder))
+                        {
+                            MessageBox.Show("目标目录\"" + strMoveFolder + parentFolder + "\"已经存在，请换一个目录");
+                            continue;
+                        }
+
+                        SQLiteCommand command = new SQLiteCommand(string.Format(sql, newPath, item.Tag), m_dbConnection);
+                        if (command.ExecuteNonQuery() > 0)
+                        {
+                            Directory.Move(sourceFolder, strMoveFolder + parentFolder);
+                            item.SubItems[1].Text = newPath;
+
+                            // 更改文件名
+                            object dataIdx = 0;
+                            string dataFormat = null;
+                            string dataName = null;
+                            string dataSql = "select id,format,name from data where book={0}";
+                            SQLiteCommand dataCmd = new SQLiteCommand(string.Format(dataSql, item.Tag), m_dbConnection);
+                            SQLiteDataReader reader = dataCmd.ExecuteReader();
+                            if (reader.Read())
+                            {
+                                dataIdx = reader["id"];
+                                dataFormat = reader["format"] as string;
+                                dataName = reader["name"] as string;
+                            }
+
+                            if (string.IsNullOrEmpty(dataFormat) || string.IsNullOrEmpty(dataName))
+                            {
+                                continue;
+                            }
+
+                            if (string.Compare(dataName, title, true) != 0)
+                            {
+                                string sourceFile = strMoveFolder + parentFolder + "/" + dataName + "." + dataFormat;
+                                string destFile = strMoveFolder + parentFolder + "/" + title + "." + dataFormat;
+                                new FileInfo(sourceFile).MoveTo(destFile);
+
+                                dataSql = "update data set name='{0}' where id = {1}";
+                                dataCmd = new SQLiteCommand(string.Format(dataSql, title, dataIdx), m_dbConnection);
+                                dataCmd.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("发生异常，" + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void CalibreTools_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            buttonClose_Click(sender, e);
+        }
+    }
+}
